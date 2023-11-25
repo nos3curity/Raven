40728e847014456e0518406c7948a2af07cc1b32
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-11-25 15:55:49 -0800

    optimized dashboard code

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -12,59 +12,13 @@ type DashboardController struct {
 
 func (c *DashboardController) Get() {
 
-	teamNetworks := make(map[int][]models.Network)
-
 	// Get all teams
 	teams, err := models.GetAllTeams()
 	if err != nil {
 		c.Ctx.WriteString(err.Error())
 	}
 
-	// Fetch the team networks
-	for _, team := range teams {
-
-		networks, err := models.GetTeamNetworks(team.Id)
-		if err != nil {
-			c.Ctx.WriteString(err.Error())
-		}
-
-		teamNetworks[team.Id] = networks
-
-	}
-
-	// Get all networks
-	networks, err := models.GetAllNetworks()
-	if err != nil {
-		c.Ctx.WriteString(err.Error())
-	}
-
-	networkSystems := make(map[string][]models.System)
-	systemPorts := make(map[string][]models.SystemPort)
-
-	// Get systems for all networks
-	for _, network := range networks {
-		systems, err := models.GetNetworkSystems(network.NetworkCidr)
-		if err != nil {
-			continue
-		}
-
-		networkSystems[network.NetworkCidr] = systems
-
-		// Grab open ports for each system
-		for _, system := range systems {
-			ports, err := models.GetSystemPorts(system.Ip)
-			if err != nil {
-				continue
-			}
-
-			systemPorts[system.Ip] = ports
-		}
-	}
-
-	c.Data["networks"] = networks
-	c.Data["network_systems"] = networkSystems
-	c.Data["system_ports"] = systemPorts
-
+	// Pass to the template
 	c.Data["teams"] = teams
 	c.Layout = "sidebar.tpl"
 	c.TplName = "dashboard.html"
