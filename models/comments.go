97eaf1fd2cf0530d98ef8a3e0b12834e5e31f07a
Brice | bri5ee <58535635+bri5ee@users.noreply.github.com>
2023-11-24 22:44:53 -0800

    Add DeleteComment function

--- models/comments.go
+++ models/comments.go
@@ -1,8 +1,9 @@
 package models
 
 import (
-	"github.com/beego/beego/v2/client/orm"
 	"time"
+
+	"github.com/beego/beego/v2/client/orm"
 )
 
 type Comment struct {
@@ -18,28 +19,35 @@ func init() {
 }
 
 func AddComment(systemIp string, username string, commentText string) error {
-    o := orm.NewOrm()
+	o := orm.NewOrm()
 
-    // Find the system by IP
-    system := System{Ip: systemIp}
-    if err := o.Read(&system, "Ip"); err != nil {
-        return err // System not found
-    }
+	// Find the system by IP
+	system := System{Ip: systemIp}
+	if err := o.Read(&system, "Ip"); err != nil {
+		return err // System not found
+	}
 
-    comment := &Comment{
-        SystemIp: &system,
-        Username: username, // Save the username with the comment
-        Text: commentText,
-    }
+	comment := &Comment{
+		SystemIp: &system,
+		Username: username, // Save the username with the comment
+		Text:     commentText,
+	}
 
-    // Insert the comment into the database
-    _, err := o.Insert(comment)
-    return err
+	// Insert the comment into the database
+	_, err := o.Insert(comment)
+	return err
 }
 
 func GetCommentsBySystemIp(systemIp string) ([]Comment, error) {
-    o := orm.NewOrm()
-    var comments []Comment
-    _, err := o.QueryTable("comment").Filter("SystemIp__Ip", systemIp).RelatedSel().All(&comments)
-    return comments, err
+	o := orm.NewOrm()
+	var comments []Comment
+	_, err := o.QueryTable("comment").Filter("SystemIp__Ip", systemIp).RelatedSel().All(&comments)
+	return comments, err
+}
+
+// DeleteComment deletes a comment from the database
+func DeleteComment(commentId int) error {
+	o := orm.NewOrm()
+	_, err := o.Delete(&Comment{Id: commentId})
+	return err
 }
