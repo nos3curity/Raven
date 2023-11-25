88df53a8bce4fe9cb00dee87f2ada1ad7ebdf120
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-11-25 10:37:04 -0800

    deprecated error func

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"fmt"
 	"raven/models"
 
 	beego "github.com/beego/beego/v2/server/web"
@@ -57,21 +56,4 @@ func (c *DashboardController) Get() {
 	c.Layout = "sidebar.tpl"
 	c.TplName = "dashboard.html"
 	return
-}
-
-func GetErrorMessage(err error) string {
-
-	defaultError := "🤓🤓🤓 OOPSIE WOOPSIE!! Uwu We make a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this! 🤓🤓🤓"
-
-	runmode, err := beego.AppConfig.String("runmode")
-	if err != nil {
-		fmt.Println("something is really fucking wrong")
-	}
-
-	if runmode == "dev" {
-		return err.Error()
-	} else {
-		return defaultError
-	}
-
 }
